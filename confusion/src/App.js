69a4973cdaf3b3c55fba323676c4850092ab1b1a
Tuan Anh <langthambca@gmail.com>
2022-04-03 22:43:00 +0700

    Components Part 1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,15 +1,17 @@
 import logo from "./logo.svg";
-import { Narbar, NavbarBrand } from "reactstrap";
+import Menu from "./Component/MenuComponent";
+import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Narbar dark color="primary">
+      <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante con Fusion</NavbarBrand>
         </div>
-      </Narbar>
+      </Navbar>
+      <Menu />
     </div>
   );
 }
