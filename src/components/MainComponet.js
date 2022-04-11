2560927da7979db9851250e56897c72d4fddf688
nstanw <langthambca@gmail.com>
2022-04-11 21:27:25 +0700

    Header and Footer

---
+++ src/components/MainComponet.js
@@ -0,0 +1,32 @@
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./MenuComponents";
+import { DISHES } from "../shared/dishes";
+import { Component } from "react";
+import DishDetail from "./DishDetailComponent";
+import Header from "./HeadderComponent";
+import Footer from "./FooterComponent";
+import React from "react";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      selectedDish: null,
+    };
+  }
+  onDishSelect(dishId) {
+    this.setState({ selectedDish: dishId });
+  }
+  render() {
+    return (
+      <div className="App">
+             <Header />
+        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Footer />
+      </div>
+    );
+  }
+}
+export default Main;
