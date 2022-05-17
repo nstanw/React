eb376732677c197dacba07fa88fe9878042ec1b4
Tuan Anh <langthambca@gmail.com>
2022-05-17 22:44:07 +0700

    reset form contacts

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import mainReducer from '../features/mainSlice'
+import mainReducer from "../features/mainSlice";
+import { createForms } from "react-redux-form";
+import { InitialFeedback } from "../Components/forms";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  main: mainReducer,
+  ...createForms({
+    feedback: InitialFeedback,
+  }),
+});
 
 export const store = configureStore({
-  reducer: {
-      main: mainReducer
-  },
+  reducer: rootReducer,
 });
